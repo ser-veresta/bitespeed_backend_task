4afe3e3369887e58cad7e3f3175d1fe09b7f3cb0
ser-veresta <subramaniang573@gmail.com>
2023-06-14 07:38:24 +0530

    Initialize Project

---
+++ src/utils/dataSource.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import { Identity } from "../entity/identity.entity";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  logging: true,
+  entities: [Identity],
+  subscribers: [],
+  migrations: [],
+});
